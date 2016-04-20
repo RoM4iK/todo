7b2694906e0d1eadea982cb27c0f1185d8698d7a
Roman <w2@live.ru>
2016-04-20 03:09:14 +0300

    shoulda specs fixed

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it { validate_presence_of :title }
-  it { belong_to :user }
+  it { should validate_presence_of :title }
+  it { should belong_to :user }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { validate_presence_of :username}
-  it { have_many :projects }
+  it { should have_many :projects }
 end
