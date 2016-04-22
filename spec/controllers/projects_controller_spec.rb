a9e366afdf8f1e565772a5e5036718b60bfd16e3
Roman <w2@live.ru>
2016-04-22 22:14:10 +0300

    projects controller update spec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -66,61 +66,131 @@ RSpec.describe ProjectsController, type: :controller do
       end
     end
   end
-  #TODO: "409 conflict" reponse
   describe 'POST #create' do
-    context "with valid project" do
+    context 'when user can create project' do
       before do
+        setup_ability
+        @user = FactoryGirl.create(:user)
+        allow(controller).to receive(:current_user) { @user }
+        @ability.can :create, Project
+      end
+      context "with valid project" do
+        before do
+          @project = FactoryGirl.build(:project)
+          post :create, project: @project.attributes, format: :json
+        end
+        it 'should create new project' do
+          expect(Project.last.uuid).to eq(@project.uuid)
+        end
+        it 'should have 201 http status' do
+          expect(response).to have_http_status(201)
+        end
+        it 'should have location header' do
+          expect(response.headers).to include("Location")
+        end
+        it 'should return created project' do
+          expect(response).to render_template(:show)
+        end
+      end
+      context "with invalid project" do
+        before do
+          @project = Project.new
+          # REVIEW: this expectation should be refactored, or no?
+          expect(Project.count).to eq(0)
+          post :create, project: @project.attributes, format: :json
+        end
+        it 'should not create new project' do
+          expect(Project.count).to eq(0)
+        end
+        it 'should have 403 http status' do
+          expect(response).to have_http_status(403)
+        end
+        it 'should return errors list' do
+          @project.validate
+          expect(JSON.parse(response.body)["errors"]).to eq(@project.errors.full_messages)
+        end
+      end
+      context "when project already created" do
+        before do
+          @project = FactoryGirl.create(:project)
+          expect(Project.count).to eq(1)
+          post :create, project: @project.attributes, format: :json
+        end
+        it 'should not create new project' do
+          expect(Project.count).to eq(1)
+        end
+        it 'should have 409 http status' do
+          expect(response).to have_http_status(409)
+        end
+        it 'should return errors list' do
+          expect(JSON.parse(response.body)["errors"]).to eq(["Uuid has already been taken"])
+        end
+      end
+    end
+    context 'when user cannot create project' do
+      before do
+        setup_ability
+        @user = FactoryGirl.create(:user)
+        allow(controller).to receive(:current_user) { @user }
+        @ability.cannot :create, Project
         @project = FactoryGirl.build(:project)
         post :create, project: @project.attributes, format: :json
       end
-      it 'should create new project' do
-        expect(Project.last.uuid).to eq(@project.uuid)
-      end
-      it 'should have 201 http status' do
-        expect(response).to have_http_status(201)
-      end
-      it 'should have location header' do
-        expect(response.headers).to include("Location")
-      end
-      it 'should return created project' do
-        expect(response).to render_template(:show)
-      end
-    end
-    context "with invalid project" do
-      before do
-        @project = Project.new
-        # REVIEW: this expectation should be refactored, or no?
-        expect(Project.count).to eq(0)
-        post :create, project: @project.attributes, format: :json
-      end
-      it 'should not create new project' do
-        expect(Project.count).to eq(0)
-      end
-      it 'should have 403 http status' do
-        expect(response).to have_http_status(403)
-      end
-      it 'should return errors list' do
-        @project.validate
-        expect(JSON.parse(response.body)["errors"]).to eq(@project.errors.full_messages)
-      end
-    end
-    context "when project already created" do
-      before do
-        @project = FactoryGirl.create(:project)
-        expect(Project.count).to eq(1)
-        post :create, project: @project.attributes, format: :json
-      end
-      it 'should not create new project' do
-        expect(Project.count).to eq(1)
-      end
-      it 'should have 409 http status' do
-        expect(response).to have_http_status(409)
-      end
-      it 'should return errors list' do
-        expect(JSON.parse(response.body)["errors"]).to eq(["Uuid has already been taken"])
-      end
+      it_behaves_like "not authorized action"
     end
   end
-  #TODO: 'PATCH #update'
+  describe 'PATCH #update' do
+    context 'when user can update project' do
+      before do
+        setup_ability
+        @user = FactoryGirl.create(:user)
+        allow(controller).to receive(:current_user) { @user }
+        @project = FactoryGirl.create(:project, {user_id: @user})
+        @ability.can :update, Project
+      end
+      context "with valid project" do
+        before do
+          @project.title = "Ninja turtles"
+          patch :update, id: @project.uuid, project: @project.attributes, format: :json
+        end
+        it 'should update project' do
+          expect(Project.find(@project.uuid).title).to eq(@project.title)
+        end
+        it 'should have 200 http status' do
+          expect(response).to have_http_status(200)
+        end
+        it 'should return updated project' do
+          expect(response).to render_template(:show)
+        end
+      end
+      context "with invalid project" do
+        before do
+          @project.title = ""
+          patch :update, id: @project.uuid, project: @project.attributes, format: :json
+        end
+        it 'should not update project' do
+          expect(Project.find(@project.uuid).title).not_to eq(@project.title)
+        end
+        it 'should have 403 http status' do
+          expect(response).to have_http_status(403)
+        end
+        it 'should return errors list' do
+          @project.validate
+          expect(JSON.parse(response.body)["errors"]).to eq(@project.errors.full_messages)
+        end
+      end
+    end
+    context 'when user cannot update project' do
+      before do
+        setup_ability
+        @user = FactoryGirl.create(:user)
+        allow(controller).to receive(:current_user) { @user }
+        @project = FactoryGirl.create(:project, {user_id: @user})
+        @ability.cannot :update, Project
+        patch :update, id: @project.uuid, project: @project.attributes, format: :json
+      end
+      it_behaves_like "not authorized action"
+    end
+  end
   #TODO: 'DELETE #destroy'
 end
